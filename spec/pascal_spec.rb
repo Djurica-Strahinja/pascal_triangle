2720d5ee6112a46b9e6b086cc3b49780b96ab121
Strahinja <djuricastrahinja@gmail.com>
2019-11-14 01:55:13 +0100

    Change tests for new draw_pascal_triangle function

--- spec/pascal_spec.rb
+++ spec/pascal_spec.rb
@@ -11,22 +11,22 @@ describe Pascal do
 
     context "run argument: 1" do
       it "returns first row of pascal triangle" do
-        sample = Pascal.new(1)
-        expect(sample.nth_row).to eql("1")
+        sample = Pascal.new
+        expect(sample.nth_row(1)).to eql("1")
       end
     end
 
     context "run argument: 5" do
       it "returns first row of pascal triangle" do
-        sample = Pascal.new(5)
-        expect(sample.nth_row).to eql("1 4 6 4 1")
+        sample = Pascal.new
+        expect(sample.nth_row(5)).to eql("1 4 6 4 1")
       end
     end
 
     context "run argument: 20" do
       it "returns first row of pascal triangle" do
-        sample = Pascal.new(10)
-        expect(sample.nth_row).to eql("1 9 36 84 126 126 84 36 9 1")
+        sample = Pascal.new
+        expect(sample.nth_row(10)).to eql("1 9 36 84 126 126 84 36 9 1")
       end
     end
   end
@@ -34,22 +34,22 @@ describe Pascal do
   describe ".draw_pascal_triangle" do
     context 'run with argument:1' do
       it 'prints first row' do
-        sample = Pascal.new(1)
-        expect { sample.draw_pascal_triangle }.to output("1\n").to_stdout
+        sample = Pascal.new
+        expect { sample.draw_pascal_triangle(1)}.to output("1\n").to_stdout
       end
     end
 
     context 'run with argument:3' do
       it 'prints first three rows' do
-        sample = Pascal.new(3)
-        expect { sample.draw_pascal_triangle }.to output("1\n1 1\n1 2 1\n").to_stdout
+        sample = Pascal.new
+        expect { sample.draw_pascal_triangle(3)}.to output("1\n1 1\n1 2 1\n").to_stdout
       end
     end
 
     context 'run with argument:5' do
       it 'prints first five rows' do
-        sample = Pascal.new(5)
-        expect { sample.draw_pascal_triangle }.to output("1\n1 1\n1 2 1\n1 3 3 1\n1 4 6 4 1\n").to_stdout
+        sample = Pascal.new
+        expect { sample.draw_pascal_triangle(5)}.to output("1\n1 1\n1 2 1\n1 3 3 1\n1 4 6 4 1\n").to_stdout
       end
     end
   end
