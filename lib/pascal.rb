e7d7ae18b3b6e2404f7df88ec20c431f777c965c
Strahinja <djuricastrahinja@gmail.com>
2019-11-14 01:54:20 +0100

    Remove unnecessary code from funcrion draw_pascal_triangle

--- lib/pascal.rb
+++ lib/pascal.rb
@@ -1,18 +1,17 @@
 
 class Pascal
-  def initialize(row_num = 1)
-    @row_num = row_num
-  end
-  def nth_row
+
+
+  def nth_row(row_num=1)
     row = [1,1]
     new_row= []
 
-    if @row_num == 1
+    if row_num == 1
       return "1"
-    elsif @row_num == 2
+    elsif row_num == 2
       return row.join(" ")
     else
-      (@row_num-2).times do
+      (row_num-2).times do
         new_row = []
         (row.length-1).times do |i|
             new_row << (row[i]+row[i+1])
@@ -26,30 +25,9 @@ class Pascal
     end
   end
 
-  def draw_pascal_triangle
-    row = [1,1]
-    new_row= []
-
-    if @row_num == 1
-      puts 1
-    elsif @row_num == 2
-      puts 1
-      puts row.join(" ")
-    else
-      puts "1"
-      puts row.join(" ")
-
-      (@row_num-2).times do
-        new_row = []
-        (row.length-1).times do |i|
-            new_row << (row[i]+row[i+1])
-        end
-        row = [1,1]
-        new_row.each do |num|
-          row.insert(1,num)
-        end
-        puts row.join(" ")
-      end
+  def draw_pascal_triangle(height=1)
+    for row in(1..height)
+      puts nth_row(row)
     end
   end
 end
