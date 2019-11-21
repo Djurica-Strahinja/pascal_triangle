b44e3a7aab6c0d8607c2408b7bbb2c351a51ec9b
Strahinja <djuricastrahinja@gmail.com>
2019-11-21 13:33:10 +0100

    Fix minor typos, add center method

--- lib/pascal.rb
+++ lib/pascal.rb
@@ -1,36 +1,31 @@
 class PascalTriangle
+
   attr_reader :first_row, :second_row, :space
   def initialize
     @first_row = [1]
     @second_row = [1,1]
     @space = " "
   end
+
   def draw(height)
-    num_spaces = height
+    max_width = nth_row(height).join(" ").length
     for row in (1..height)
-      print_row(num_spaces, row)
-      num_spaces -= 1
+     puts (nth_row(row).join(" ")).center(max_width)
     end
   end
 
   private
-  def print_row(num_spaces, row)
-    puts "#{@space*num_spaces}#{nth_row(row).join(" ")}"
-  end
-
   def nth_row(row_num)
-    return @first_row  if row_num == 1
-    return @second_row if row_num == 2
-    row = @second_row
+    return first_row  if row_num == 1
+    return second_row if row_num == 2
+    row = second_row
     (row_num - 2).times { row = next_row(row) }
     row
   end
 
   def next_row(row)
-    new_row = [1,1]
-    (row.length - 1).times do |i|
-      new_row.insert(1,(row[i] + row[i+1]))
-    end
+    new_row = [1, 1]
+    (row.length - 1).times {|i| new_row.insert(1,(row[i] + row[i + 1])) }
     new_row
   end
 end
