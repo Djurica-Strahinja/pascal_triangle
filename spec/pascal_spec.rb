0d26dea11ed64e99c781281fe6910f5f82eaf20f
Strahinja <djuricastrahinja@gmail.com>
2019-11-21 13:33:49 +0100

    Fix tests to pass according to changes

--- spec/pascal_spec.rb
+++ spec/pascal_spec.rb
@@ -5,28 +5,28 @@ describe PascalTriangle do
     context 'with argument: 0' do
       it 'prints first row' do
         sample = PascalTriangle.new
-        expect { sample.draw(1)}.to output(" 1\n").to_stdout
+        expect { sample.draw(1)}.to output("1\n").to_stdout
       end
     end
 
     context 'with argument: 1' do
       it 'prints first row' do
         sample = PascalTriangle.new
-        expect { sample.draw(1)}.to output(" 1\n").to_stdout
+        expect { sample.draw(1)}.to output("1\n").to_stdout
       end
     end
 
     context 'with argument: 3' do
       it 'prints first three rows' do
         sample = PascalTriangle.new
-        expect { sample.draw(3)}.to output("   1\n  1 1\n 1 2 1\n").to_stdout
+        expect { sample.draw(3)}.to output("  1  \n 1 1 \n1 2 1\n").to_stdout
       end
     end
 
     context 'with argument: 5' do
       it 'prints first five rows' do
         sample = PascalTriangle.new
-        expect { sample.draw(5)}.to output("     1\n    1 1\n   1 2 1\n  1 3 3 1\n 1 4 6 4 1\n").to_stdout
+        expect { sample.draw(5)}.to output("    1    \n   1 1   \n  1 2 1  \n 1 3 3 1 \n1 4 6 4 1\n").to_stdout
       end
     end
   end
